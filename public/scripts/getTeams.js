421b7ec739549dbf83469607fa307bc907f69742
Walter Melo <41174751+walmello@users.noreply.github.com>
2025-07-24 02:07:26 -0300

    modificou o banco que a API aponta

--- public/scripts/getTeams.js
+++ public/scripts/getTeams.js
@@ -1,6 +1,6 @@
 async function buscarTimes(){
-    const url = 'https://api.baserow.io/api/database/rows/table/597821/?user_field_names=true'
-    const token = 'vt2Fh24HCvHdIcsEzD6kKoHPYLHLwy2a'
+    const url = 'https://api.baserow.io/api/database/rows/table/617416/?user_field_names=true'
+    const token = '2y7lVz5XeO4M0TllXMdxUcjlUditP5IA'
 
     const result = await fetch(url, {
         headers: {
@@ -9,7 +9,7 @@ async function buscarTimes(){
     })
 
     const data = await result.json();
-    const times = await data.results;
+    const times = await data.results.filter(el => el.Nome);
     const ordered = await times.sort((a, b) => a.Pontos - b.Pontos).reverse()
     return ordered
 }
@@ -17,8 +17,8 @@ async function buscarTimes(){
 async function buscarTime(){
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('id');
-    const url = `https://api.baserow.io/api/database/rows/table/597821/${id}/?user_field_names=true`
-    const token = 'vt2Fh24HCvHdIcsEzD6kKoHPYLHLwy2a'
+    const url = `https://api.baserow.io/api/database/rows/table/617416/${id}/?user_field_names=true`
+    const token = '2y7lVz5XeO4M0TllXMdxUcjlUditP5IA'
     const result = await fetch(url, {
         headers: {
             Authorization: "Token " + token
