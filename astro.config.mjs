c9bc787d9b0b358685555c5d9b4838eb7ea79ae7
Walter Melo <41174751+walmello@users.noreply.github.com>
2025-07-25 21:31:47 -0300

    consertar as imagens que faltam no URL css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath, URL } from 'node:url';
 
-// https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      }
+    }
+  }
+});
